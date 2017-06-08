bf841b5ae29bcc8197b3382ef33448810c01eda4
张朋 <zhangpeng@data-god.com>
2017-06-08 17:54:45 +0800

    list.js

--- src/config/list.js
+++ src/config/list.js
@@ -1,17 +1,32 @@
 export default {
   articleList: [{
     title: 'ARTICLE',
-    img: 'assets/logo.png',
+    img: '../assets/logo.png',
     time: '2017-06-06',
     info: 'Making Photos Smaller Without Quality Loss'
   }, {
     title: 'ARTICLE',
-    img: '/assets/logo.png',
+    img: '../assets/logo.png',
     time: '2017-06-06',
     info: 'Making Photos Smaller Without Quality Loss'
   }, {
     title: 'ARTICLE',
-    img: '/assets/logo.png',
+    img: '../assets/logo.png',
+    time: '2017-06-06',
+    info: 'Making Photos Smaller Without Quality Loss'
+  },{
+    title: 'ARTICLE',
+    img: '../assets/logo.png',
+    time: '2017-06-06',
+    info: 'Making Photos Smaller Without Quality Loss'
+  }, {
+    title: 'ARTICLE',
+    img: '../assets/logo.png',
+    time: '2017-06-06',
+    info: 'Making Photos Smaller Without Quality Loss'
+  }, {
+    title: 'ARTICLE',
+    img: '../assets/logo.png',
     time: '2017-06-06',
     info: 'Making Photos Smaller Without Quality Loss'
   }]
